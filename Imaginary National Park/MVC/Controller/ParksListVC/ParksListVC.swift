9f5b918d8ae03d677dec2559d0d3aaf6aeac0c3f
Marwen Doukh <marwen.doukh@gmail.com>
2020-01-07 21:31:42 +0100

    delete SB

---
+++ Imaginary National Park/MVC/Controller/ParksListVC/ParksListVC.swift
@@ -0,0 +1,18 @@
+//
+//  ParksListVC.swift
+//  Imaginary National Park
+//
+//  Created by Marwen Doukh on 07.01.20.
+//  Copyright © 2020 Marwen Doukh. All rights reserved.
+//
+
+import UIKit
+
+class ParksListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
